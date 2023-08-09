2f241c1cfc3da1ccd76e5a6f95466e5cc8deef99
Dhiyanesh <dhiyaneshsasi03@gmail.com>
2023-08-09 18:48:26 +0530

    user auth state

--- lib/main.dart
+++ lib/main.dart
@@ -1,45 +1,49 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:mapsss/presentation/screens/common/nav_bar/nav_bar.dart';
-
 import 'package:mapsss/presentation/screens/home/welcome_page.dart';
-import 'package:mapsss/presentation/screens/map_view/test_map.dart';
-
-
 import 'presentation/screens/display_parking_details/DisplayParkingData_page.dart';
 import 'presentation/screens/parking_details_form/ParkingForm_page.dart';
-import 'presentation/screens/parking_Details/details_page.dart';
-
 import 'presentation/screens/settings/settings_page.dart';
 import 'presentation/auth/register_page.dart';
 import 'presentation/screens/about/about.dart';
-import 'presentation/screens/loading/loading_screen.dart';
 import 'presentation/auth/login_page.dart';
 import 'presentation/screens/home/simple_starting_screen.dart';
 import 'presentation/screens/map_view/parking_map_view.dart';
-
 import 'package:firebase_core/firebase_core.dart';
-
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(const ParkingLocatorApp());
+
+  final bool userLoggedIn = isLoggedIn();
+
+  runApp(ParkingLocatorApp(initialRoute: userLoggedIn ? '/navBar' : '/welcomePage'));
 }
 
+
+
+// Check if the user is logged in
+bool isLoggedIn() {
+  return FirebaseAuth.instance.currentUser != null;
+}
+
+
 class ParkingLocatorApp extends StatelessWidget {
-  const ParkingLocatorApp({super.key});
+  final String initialRoute;
+
+  const ParkingLocatorApp({required this.initialRoute, super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: WelcomePage(),
+      initialRoute: initialRoute,
       routes: {
         '/home' : (context) => SimpleStartingScreen(),
         '/welcomePage' : (context) => WelcomePage(),
         '/mapView': (context) => ParkingMapView(),
         '/aboutUs': (context) => AboutPage(),
-        // '/parkingsPage': (context) => ParkingsPage(),
         '/login': (context) => LoginPage(),
         '/register': (context) => RegisterPage(),
         '/navBar': (context) => BottomNavigationBarPage(),
@@ -52,3 +56,57 @@ class ParkingLocatorApp extends StatelessWidget {
     );
   }
 }
+
+
+
+
+
+// import 'package:flutter/material.dart';
+// import 'package:mapsss/presentation/screens/common/nav_bar/nav_bar.dart';
+// import 'package:mapsss/presentation/screens/home/welcome_page.dart';
+// import 'presentation/screens/display_parking_details/DisplayParkingData_page.dart';
+// import 'presentation/screens/parking_details_form/ParkingForm_page.dart';
+// import 'presentation/screens/settings/settings_page.dart';
+// import 'presentation/auth/register_page.dart';
+// import 'presentation/screens/about/about.dart';
+// import 'presentation/auth/login_page.dart';
+// import 'presentation/screens/home/simple_starting_screen.dart';
+// import 'presentation/screens/map_view/parking_map_view.dart';
+// import 'package:firebase_core/firebase_core.dart';
+//
+//
+// void main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   await Firebase.initializeApp();
+//   runApp(const ParkingLocatorApp());
+// }
+//
+// class ParkingLocatorApp extends StatelessWidget {
+//   const ParkingLocatorApp({super.key});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       home: WelcomePage(),
+//       routes: {
+//         '/home' : (context) => SimpleStartingScreen(),
+//         '/welcomePage' : (context) => WelcomePage(),
+//         '/mapView': (context) => ParkingMapView(),
+//         '/aboutUs': (context) => AboutPage(),
+//         '/login': (context) => LoginPage(),
+//         '/register': (context) => RegisterPage(),
+//         '/navBar': (context) => BottomNavigationBarPage(),
+//         '/form': (context) => FormPage(),
+//         '/displayPage': (context) => DisplayPage(),
+//
+//         '/settingsPage': (context) => SettingsPage()
+//
+//       },
+//     );
+//   }
+// }
+
+
+
+
